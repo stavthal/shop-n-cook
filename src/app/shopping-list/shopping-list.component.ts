3f29a766b3537f252e1d9e2d6d28561add967858
Stavros Thalassinos <stavros8003@gmail.com>
2024-05-13 22:49:56 +0300

    added ingredients display

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Model
+import { Ingredient } from '../shared/ingredient-model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
 
+  ingredient_1 = new Ingredient('Apples', 5);
+  ingredient_2 = new Ingredient('Tomatoes', 10);
+
+  constructor() {}
+
+  ngOnInit() {
+    this.ingredients.push(this.ingredient_1);
+    this.ingredients.push(this.ingredient_2);
+  }
 }
