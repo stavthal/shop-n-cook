6aa96334282c20668c8ac1a7a3d74fe1bf9e7a51
Stavros Thalassinos <stavros8003@gmail.com>
2024-05-13 21:22:36 +0300

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Model
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = []; // Making an array of Recipes that follow the recipe model
 
+  test_recipe: Recipe = new Recipe('A Test Recipe', 'This is simply a test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMFAN6MNhsIFJX1arZEqkxdG5-S7alw0isyawaaSKQjg&s' );
+  constructor() {
+  }
+  ngOnInit() {
+    this.recipes.push(this.test_recipe);
+  }
 }
