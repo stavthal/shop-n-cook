08e20343ff73a52a5fe68d0661b7de778a5bf85d
Stavros Thalassinos <stavros8003@gmail.com>
2024-05-19 15:51:49 +0300

    event binding for recipe selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 // Model
-import {Recipe} from "../recipe.model";
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = []; // Making an array of Recipes that follow the recipe model
 
-  test_recipe: Recipe = new Recipe('A Test Recipe', 'This is simply a test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMFAN6MNhsIFJX1arZEqkxdG5-S7alw0isyawaaSKQjg&s' );
-  constructor() {
+  test_recipe: Recipe = new Recipe(
+    'A Test Recipe',
+    'This is simply a test description',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMFAN6MNhsIFJX1arZEqkxdG5-S7alw0isyawaaSKQjg&s',
+  );
+
+  constructor() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
+
   ngOnInit() {
     this.recipes.push(this.test_recipe);
   }
