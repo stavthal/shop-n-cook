10a75a01d6a168a4bf58e9ea32408497e8a88473
Stavros Thalassinos <stavros8003@gmail.com>
2024-05-19 17:30:45 +0300

    added selected recipe details

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
+  styleUrl: './recipe-detail.component.css',
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
 
+  constructor() {}
+
+  ngOnInit() {}
 }
